19d21b6bc9cc8c3b9e742add6035274fefd016b5
Vigorass <qqcao.cs@gmail.com>
2015-01-07 02:01:59 +0800

    update 1007 java version

--- src/p1006/Main.java
+++ src/p1006/Main.java
@@ -2,22 +2,19 @@ package p1006;
 
 import java.util.Scanner;
 
-/* Copyright(C) 2014,Cao Qingqing , Wuhan University
- * All rights reserved.
- *	
- * This class is submitted for solving the problem 1006 
- * in WOJ
+/**
+ * Copyright(C) 2015,Cao Qingqing , Wuhan University All rights reserved.
  * 
- * Author: Qingqing Cao, Computer School, Wuhan University
- * Time: 2014-12-27
- * Email: im@caoqq.net
+ * This class is submitted for solving the problem 1006 in WOJ
+ * 
+ * @author : Qingqing Cao, Computer School, Wuhan University Time: 201-01-06
+ * 
  */
 public class Main {
 
-	private int bfsCount(int[][] graph, int start, int end) {
+	private static int bfsCount(int[][] graph, int start, int end) {
 		if (start == end)
 			return 0;
-
 		int[] visited = new int[graph.length];
 
 		int[] queue = new int[graph.length];
@@ -26,44 +23,52 @@ public class Main {
 		}
 
 		int breadth = -1;// the breadth of the graph
+
 		int k = 0;// the number of the total elements in the queue
 		queue[k++] = start;
 
 		int i;
+		int flag = 1;
+		int top = 0;
+		int pos = 1;
 		while (k > 0) {
-			if (queue[0] == end) {
+
+			if (queue[top] == end) {
 				return breadth;
 			} else {
-				i = queue[0];
-				breadth++;
+				i = queue[top];
+				visited[i] = 1;
+				if (flag == 0) {
+					flag = k;
+				}
+				flag--;
+				k--;
+				top++;
 			}
 
-			k--;
-			int r = 0;
-			do {
-				queue[r] = queue[r + 1];
-				r++;
-			} while (r < k);
-
-			if (visited[i] != 1) {
-				visited[i] = 1;
-
-				for (int j = 0; j < graph[i].length; j++) {
-					if (graph[i][j] == 1 && visited[j] != 1) {
-						queue[k++] = j;
-
-					}
+			for (int j = 0; j < graph[i].length; j++) {
+				if (graph[i][j] == 1 && visited[j] != 1) {
+					queue[pos++] = j;
+					k++;
+					visited[j] = 1;
 				}
 			}
 
+			if (flag == 0) {
+				breadth++;
+				flag = k;
+			}
 		}
 
-		return breadth;
+		if (visited[end] != 1)
+			return -1;
+		else
+			return breadth;
 	}
 
-	private void printResult() {
-		Scanner input = new Scanner(System.in);
+	public static void main(String args[]) {
 
+		Scanner input = new Scanner(System.in);
 		int animalNo = input.nextInt();
 		int[][] animalGraph = new int[animalNo][animalNo];
 		int pairsNo = input.nextInt();
@@ -75,6 +80,7 @@ public class Main {
 			animalGraph[from][to] = 1;
 			animalGraph[to][from] = 1;
 		}
+		// long tic = System.currentTimeMillis();
 		int queryTimes = input.nextInt();
 		for (int i = 0; i < queryTimes; i++) {
 			from = input.nextInt();
@@ -82,12 +88,10 @@ public class Main {
 
 			System.out.println(bfsCount(animalGraph, from, to));
 		}
+		// long toc = System.currentTimeMillis();
 
 		input.close();
-	}
-
-	public static void main(String args[]) {
-		new Main().printResult();
+		// System.out.println(toc - tic);
 	}
 
 }
---
+++ src/p1007/Main.java
@@ -0,0 +1,42 @@
+package p1007;
+
+import java.util.Scanner;
+
+/**
+ * Copyright(C) 2015,Cao Qingqing , Wuhan University All rights reserved.
+ * 
+ * This class is submitted for solving the problem 1006 in WOJ
+ * 
+ * @author : Qingqing Cao, Computer School, Wuhan University Time: 201-01-06
+ * 
+ */
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext()) {
+			int animalNo = input.nextInt();
+			int[][] feedTime = new int[8][animalNo];
+			int totalTime = 0;
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < animalNo; j++) {
+					feedTime[i][j] = input.nextInt();
+				}
+			}
+			int tmp = 0;
+			for (int j = 0; j < animalNo; j++) {
+				for (int i = 0; i < 7; i++) {
+					if (feedTime[i][j] < feedTime[i + 1][j]) {
+						tmp = feedTime[i][j];
+						feedTime[i][j] = feedTime[i + 1][j];
+						feedTime[i + 1][j] = tmp;
+					}
+				}
+
+				totalTime += feedTime[7][j];
+
+			}
+			System.out.println(totalTime);
+		}
+		input.close();
+	}
+}
