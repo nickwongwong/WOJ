47f53264d2f5df1a362cab07a09744839e360291
Vigorass <qqcao.cs@gmail.com>
2015-01-09 01:06:09 +0800

    update 1010

---
+++ src/p1012/Main.java
@@ -0,0 +1,21 @@
+package p1012;
+
+import java.util.Scanner;
+
+/**
+ * Copyright(C) 2015,Cao Qingqing , Wuhan University All rights reserved.
+ * 
+ * This class is submitted for solving the problem 1012 in WOJ
+ * 
+ * @author : Qingqing Cao, Computer School, Wuhan University
+ * 
+ */
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		while (input.hasNext()) {
+
+		}
+		input.close();
+	}
+}
