2a996cfcf0a1cb662f63e2abd86b94c14c61e877
Vigorass <qqcao.cs@gmail.com>
2015-01-16 01:18:37 +0800

    update 1295

--- src/p1295/Main.c
+++ src/p1295/Main.c
@@ -2,46 +2,48 @@
 #include<stdio.h>
 #include<string.h>
 int bracketMatch(char a,char b){
+	int flag=0;
 	switch (a){
-		case '(': if(b==')') return 1;
-		          else return 0;
-		case '[': if(b==']') return 1;
-		          else return 0;
-		case '{': if(b=='}') return 1;
-		          else return 0;
+		case '(': if(b==')') flag= 1;break;
+
+		case '[': if(b==']') flag= 1;break;
+		case '{': if(b=='}')flag= 1;break;
 	}
+	return flag;
 }
 int main()
 {
     int n=0,i=0;
  	freopen("test.txt","r",stdin);
 	//freopen("result.txt","w",stdout);
-	char a[1002],b[1002];
+
 	scanf("%d",&n);
 	while(n-->0){
-		scanf("%s",&a);
-		
+		i++;
+		char a[1010],b[1010];
+		scanf("%s",a);
+
 		int len=strlen(a);
 		int j=0,count=0;
+		b[0]='$';
 		for(j=0;j<len;j++)
 		{
 			if(a[j]=='{'||a[j]=='}'||a[j]=='('||a[j]==')'||a[j]=='['||a[j]==']')
 			{
-				if(!suit(b[loop],a[j]))
-				{
-					loop++;
-					b[loop]=a[j];
+				if(!bracketMatch(b[count],a[j])){
+
+					b[++count]=a[j];
 				}
-				else loop--;
+
+				else {
+				count--;
+
+				}
 			}
 		}
-		if(loop==0) printf("Case %d: Legal\n",i+1);
-		else printf("Case %d: Illegal\n",i+1);
-		for(i=0;i<len;i++){
-			if(!proteinMatch(str1[i],str2[i]))
-			sum++;
-		}
-		printf("%d\n",sum);
+		if(count==0) printf("Case %d: Legal\n",i);
+		else printf("Case %d: Illegal\n",i);
+
 	}
 
 	return 0;
